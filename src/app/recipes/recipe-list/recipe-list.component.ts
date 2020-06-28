9403187f55f3b1d6b48c48d100eceeb2fcb9999f
David Romero Estrada <dmail.earth@gmail.com>
2020-06-28 13:42:39 -0500

    agregando componentes nuevos

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,7 +8,16 @@ import { Recipe } from '../recipe';
 })
 export class RecipeListComponent implements OnInit {
 
-  recipes: Recipe[] = []; 
+  recipes: Recipe[] = [{
+    name: "Receta 1",
+    description: "pasta 1",
+    imagePath: "https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/caponata-pasta_1.jpg"
+  },{
+    name: "Receta 2",
+    description: "pasta 2",
+    imagePath: "https://static01.nyt.com/images/2020/02/10/dining/onepot-cheesypasta/onepot-cheesypasta-articleLarge.jpg"
+  }];
+
   constructor() { }
 
   ngOnInit(): void {
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+
+  ingredients:Ingredient[] = [{ name:"Apple", amount: 12 }, {name:"Tomates", amount: 12}];
 
   constructor() { }
 
